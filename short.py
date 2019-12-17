5a7a4640f0db59e5211f6fb48174255b1ee8659c
Ilya Rayko <mylntsa.ilya.63@gmail.com>
2019-12-17 22:49:10 +0300

    Конструктор

---
+++ short.py
@@ -0,0 +1,5 @@
+Float = float
+
+
+class A:
+    pass
--- test.py
+++ test.py
@@ -1,3 +1,44 @@
+"""Module with Float class."""
+from typing import *
+from abc import abstractmethod
+from numbers import Real
+import sys
+import random
+
+
+class binary64(Real):
+    """ABC for Floats."""
+
+    @abstractmethod
+    def as_integer_ratio(self) -> Tuple[int, int]:
+        """Return a pair of integers whose ratio is exactly equal to the original float."""
+        pass
+
+    @abstractmethod
+    def is_integer(self) -> bool:
+        """Return True if the float instance is finite with integral value."""
+        pass
+
+    @abstractmethod
+    def hex(self) -> str:
+        """Return a representation of a floating-point number as a hexadecimal string."""
+        pass
+
+    @classmethod
+    @abstractmethod
+    def fromhex(cls, s: str) -> "binary64":
+        """Class method to return the float represented by a hexadecimal string s."""
+        pass
+
+
+binary64.register(float)
+
+
+Float = float
+
+
+class A:
+    pass
 
 
 passed = True
